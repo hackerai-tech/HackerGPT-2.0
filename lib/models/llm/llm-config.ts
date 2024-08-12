42dc043a481feed7613f38df536a4d387ef8a3bf
thehackergpt <contact@hackergpt.chat>
2024-08-11 20:56:44 -0400

    Add browser tool to PGPT-3.5

--- lib/models/llm/llm-config.ts
+++ lib/models/llm/llm-config.ts
@@ -36,7 +36,7 @@ const llmConfig = {
     // For Hacker RAG
     RAG: `${initialSystemPrompt} ${process.env.RAG_SYSTEM_PROMPT}\n${currentDate}`,
     // For PGPT-3.5
-    pgpt35WithTools: `${getPentestGPTInfo(initialSystemPrompt)}\n${getPentestGPTToolsInfo()}\n${getPentestGPTSystemPromptEnding}`,
+    pgpt35WithTools: `${getPentestGPTInfo(initialSystemPrompt)}\n${getPentestGPTToolsInfo(false, true)}\n${getPentestGPTSystemPromptEnding}`,
     // For PGPT-4
     pentestGPTChat: `${getPentestGPTInfo(initialSystemPrompt)}\n${getPentestGPTSystemPromptEnding}`,
     // For GPT-4o
