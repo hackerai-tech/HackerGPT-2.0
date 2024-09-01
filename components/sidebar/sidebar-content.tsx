e7dd94b7255c38bf0d415e4ce4b0e6de3236ddb1
thehackergpt <contact@hackergpt.chat>
2024-09-01 10:21:16 -0400

    Remove Sidebar search

--- components/sidebar/sidebar-content.tsx
+++ components/sidebar/sidebar-content.tsx
@@ -1,8 +1,7 @@
 import { ContentType, DataListType } from "@/types"
-import { FC, useState } from "react"
+import { FC } from "react"
 import { SidebarCreateButtons } from "./sidebar-create-buttons"
 import { SidebarDataList } from "./sidebar-data-list"
-import { SidebarSearch } from "./sidebar-search"
 
 interface SidebarContentProps {
   contentType: ContentType
@@ -13,12 +12,6 @@ export const SidebarContent: FC<SidebarContentProps> = ({
   contentType,
   data
 }) => {
-  const [searchTerm, setSearchTerm] = useState("")
-
-  const filteredData: any = data.filter(item =>
-    item.name.toLowerCase().includes(searchTerm.toLowerCase())
-  )
-
   return (
     <div className="flex max-h-[calc(100%-10px)] grow flex-col">
       <div className="flex items-center">
@@ -28,15 +21,7 @@ export const SidebarContent: FC<SidebarContentProps> = ({
         />
       </div>
 
-      <div className="mt-2">
-        <SidebarSearch
-          contentType={contentType}
-          searchTerm={searchTerm}
-          setSearchTerm={setSearchTerm}
-        />
-      </div>
-
-      <SidebarDataList contentType={contentType} data={filteredData} />
+      <SidebarDataList contentType={contentType} data={data} />
     </div>
   )
 }
