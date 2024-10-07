4fed95d15ad0a2a0b18b6cef87a92b6cef918010
thehackergpt <contact@hackergpt.chat>
2024-10-07 19:06:07 -0400

    build: update packages

--- lib/server/stripe.ts
+++ lib/server/stripe.ts
@@ -6,7 +6,7 @@ export function getStripe(): Stripe {
     throw new Error("Missing Stripe API key")
   }
   const stripe = new Stripe(apiKey, {
-    apiVersion: "2024-06-20"
+    apiVersion: "2024-09-30.acacia"
   })
   return stripe
 }
