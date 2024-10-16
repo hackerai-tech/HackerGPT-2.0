bea2d4e779a4c15c15b13ac2b7f46b3886c6888d
Rostyslav Manko <contact@hackerai.co>
2024-10-15 21:01:28 -0400

    fix: getModerationResult message check

--- app/api/chat/mistral/route.ts
+++ app/api/chat/mistral/route.ts
@@ -57,7 +57,7 @@ export async function POST(request: Request) {
 
     const { moderationLevel, shouldUncensorResponse } =
       await getModerationResult(
-        filterTargetMessage,
+        filterTargetMessage.content,
         llmConfig.openai.apiKey || ""
       )
 
