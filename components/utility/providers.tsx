5f2dd11114765ed38d7bd89ff76a638ccbbc9944
Rostyslav Manko <contact@hackerai.co>
2024-11-04 14:57:36 -0500

    build: update packages

--- components/utility/providers.tsx
+++ components/utility/providers.tsx
@@ -2,8 +2,7 @@
 
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { AlertProvider } from "@/context/alert-context"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from "next-themes"
 import { FC } from "react"
 
 export const Providers: FC<ThemeProviderProps> = ({ children, ...props }) => {
