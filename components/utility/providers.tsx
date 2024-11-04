d457223afdf6a1177c232e1d4b9ae592e0351e87
Rostyslav Manko <contact@hackerai.co>
2024-11-04 15:10:06 -0500

    refactor: remove image-picker

--- components/utility/providers.tsx
+++ components/utility/providers.tsx
@@ -2,7 +2,10 @@
 
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { AlertProvider } from "@/context/alert-context"
-import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from "next-themes"
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps
+} from "next-themes"
 import { FC } from "react"
 
 export const Providers: FC<ThemeProviderProps> = ({ children, ...props }) => {
