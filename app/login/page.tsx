3735fe6458a997cffdd5262e94bcf6c30fdf243d
Rostyslav Manko <contact@hackerai.co>
2025-01-20 12:31:32 -0500

    revert: resolve cookies request scope error in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -82,23 +82,29 @@ export default async function Login({
     errorMessage = errorMessages.ratelimit_defaul
   }
 
-  const supabase = await createClient()
-  const {
-    data: { user }
-  } = await supabase.auth.getUser()
+  const checkAuth = async () => {
+    "use server"
 
-  if (user) {
-    const { data: homeWorkspace } = await supabase
-      .from("workspaces")
-      .select("*")
-      .eq("user_id", user.id)
-      .eq("is_home", true)
-      .single()
+    const supabase = await createClient()
+    const {
+      data: { user }
+    } = await supabase.auth.getUser()
 
-    if (homeWorkspace) {
-      return redirect(`/${homeWorkspace.id}/chat`)
+    if (user) {
+      const { data: homeWorkspace } = await supabase
+        .from("workspaces")
+        .select("*")
+        .eq("user_id", user.id)
+        .eq("is_home", true)
+        .single()
+
+      if (homeWorkspace) {
+        return redirect(`/${homeWorkspace.id}/chat`)
+      }
     }
   }
+
+  await checkAuth()
 
   const signIn = async (formData: FormData) => {
     "use server"
