65d68093a6a966e767a08c5c518680c2597c5c24
Foad Kesheh <foad.kesheh@aurea.com>
2025-02-16 08:29:44 -0300

    chore: removing duplicated function

--- supabase/migrations/20250215113707_add_messages_id_to_files.sql
+++ supabase/migrations/20250215113707_add_messages_id_to_files.sql
@@ -2,5 +2,8 @@
 ALTER TABLE files
 ADD COLUMN message_id UUID REFERENCES messages(id) ON DELETE CASCADE;
 
+-- Drop function for deleting messages (duplicated)
+DROP FUNCTION IF EXISTS delete_message_including_and_after;
+
 -- Create index on message_id
 CREATE INDEX idx_files_message_id ON files(message_id);
